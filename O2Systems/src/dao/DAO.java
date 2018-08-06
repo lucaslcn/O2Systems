745409ffb0d333b7fc1a0123c2160cbdf6ab5f41
Anderson Caye <andersoncaye@gmail.com>
2018-08-06 19:50:13 -0300

    dao criado

---
+++ O2Systems/src/dao/DAO.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+/**
+ *
+ * @author anderson.caye
+ */
+public class DAO {
+    
+    public String insert(Object o){
+        
+    }
+    
+    public Object select(String){
+        return new Object();
+    }
+    
+}
