906169be54adc118097913d2323c1feb1daafd75
Anderson Caye <andersoncaye@gmail.com>
2018-08-08 21:48:39 -0300

    inclusao de biblioteca

---
+++ O2Systems/src/dao/PlanoSaudeDAO.java
@@ -0,0 +1,18 @@
+package dao;
+
+import javax.swing.JTable;
+import negocio.Pessoa;
+import persistencia.IDAO_T;
+
+/**
+ *
+ * @author elias.flach
+ */
+public class PlanoSaudeDAO implements IDAO_T<Pessoa>{
+
+    @Override
+    public void preencherTabelaBusca(JTable tabela, String criterio) {
+    
+    }
+
+}
