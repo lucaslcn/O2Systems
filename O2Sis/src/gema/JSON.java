d81ffab760680df54c34204afad43fe0ca8b7b06
Anderson Caye <andersoncaye@gmail.com>
2018-11-06 09:17:19 -0200

    help no Json

--- O2Sis/src/gema/JSON.java
+++ O2Sis/src/gema/JSON.java
@@ -1,14 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gema;
 
-/**
- *
- * @author Lucas
- */
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import static javafx.scene.input.KeyCode.T;
+import negocio.*;
+
+/* @author Lucas e andesoncaye(XorGamer)*/
 public class JSON {
-    
+
+    public static String gerarJSON(Object o, Class valueType) {
+        String codeJson = "";
+        if (valueType != null) {
+            ObjectMapper mapper = new ObjectMapper();
+            try {
+                
+                if (valueType == AgendamentoExames.class) {
+                    AgendamentoExames k = (AgendamentoExames) o;
+                    codeJson = mapper.writeValueAsString(k);
+                } else if (valueType == Consultas.class) {
+                    Consultas k = (Consultas) o;
+                    codeJson = mapper.writeValueAsString(k);
+                }
+                
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+
+        }
+        return codeJson;
+    }
 }
