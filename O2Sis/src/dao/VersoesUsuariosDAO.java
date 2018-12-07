2424fec8cdc37e5792d5ad73af55276a33df7fe2
Elias Antônio Flach <eliasflach@outlook.com>
2018-12-06 22:15:50 -0200

    Versoes

--- O2Sis/src/dao/VersoesUsuariosDAO.java
+++ O2Sis/src/dao/VersoesUsuariosDAO.java
@@ -5,12 +5,25 @@
  */
 package dao;
 
+import javax.swing.JTable;
+import negocio.UsuariosVersoes;
 import persistencia.DAO;
+import persistencia.IDAO_T;
 
 /**
  *
  * @author elias.flach
  */
-public class VersoesUsuariosDAO extends DAO {
+public class VersoesUsuariosDAO extends DAO implements IDAO_T<UsuariosVersoes>{
+
+    @Override
+    public void preencherTabelaBusca(JTable tabela, String criterio) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public UsuariosVersoes consultarId(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
