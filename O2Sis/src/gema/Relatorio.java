ef8da8ccda7f7de301aacfe2a67f0af32efd1902
Anderson Caye <andersoncaye@gmail.com>
2018-12-09 12:13:23 -0200

    relatórios bibliotecas e funcoes

---
+++ O2Sis/src/gema/Relatorio.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gema;
+
+import java.sql.Connection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.swing.JOptionPane;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.view.JasperViewer;
+import persistencia.ConexaoDAO;
+
+/**
+ *
+ * @author XorNOTE
+ */
+public class Relatorio {
+    
+    public void gerarListagem(String caminho, List tabela){
+        try {
+            // Compila o relatorio
+            JasperReport relatorio = JasperCompileManager.compileReport(getClass().getResourceAsStream("/relatorios/"+caminho));
+
+            // Mapeia campos de parametros para o relatorio, mesmo que nao existam
+            Map parametros = new HashMap();
+
+            // Executa relatoio
+            JasperPrint impressao = JasperFillManager.fillReport(relatorio, parametros, new JRBeanCollectionDataSource(tabela));
+
+            // Exibe resultado em video
+            JasperViewer.viewReport(impressao, false);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro ao gerar relatório: " + e);
+        }
+    }
+    
+    public void gerarRelatorio(String caminho, HashMap parametros, List tabela){
+        try {
+            // Compila o relatorio
+            JasperReport relatorio = JasperCompileManager.compileReport(getClass().getResourceAsStream("/relatorios/"+caminho));
+
+            // Mapeia campos de parametros para o relatorio, mesmo que nao existam
+            HashMap param = parametros;
+
+//            // adiciona parametros
+//            param.put("dataInicial", dataInicial.getText());
+//            param.put("dataFinal", dataFinal.getText());
+
+            // Executa relatoio
+            JasperPrint impressao = JasperFillManager.fillReport(relatorio, param, new JRBeanCollectionDataSource(tabela) );
+
+            // Exibe resultado em video
+            JasperViewer.viewReport(impressao, false);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro ao gerar relatório: " + e);
+        }
+    }
+    
+}
